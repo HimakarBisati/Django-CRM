3ba57ff4ddcc109cf2beb40e5361f1a70dc7a4a9
Himakar Bisati <163105328+HimakarBisati@users.noreply.github.com>
2024-12-15 11:41:23 +0530

    Add files via upload

---
+++ Django-CRM-main/website/urls.py
@@ -0,0 +1,17 @@
+
+from django.urls import path
+from . import views 
+
+urlpatterns = [
+    path('',views.home,name = 'home'),
+    path('logout/',views.logout_user,name = 'logout'),
+    path('register/',views.register_user,name = 'register'),
+    path('records/',views.Records_view, name="records"),
+    path('manage_records/',views.manage_Records, name="manage_records"),
+    path('view_person/<int:pk>/',views.view_person, name="view_person"),
+    path('add_records/',views.add_records, name="add_records"),
+    path('delete_records/<int:pk>/',views.delete_records, name="delete_records"),
+    path('update_records/<int:pk>/',views.edit_records, name="update_records"),
+
+    
+]
